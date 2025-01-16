315bb7aa8bf7c1ddf5f0ce151c3bf3ecc21d5640
Stefbls <stefano.bilosi@gmail.com>
2025-01-16 17:53:38 +0100

    Update calcolo_pressioni.py

--- calcolo_pressioni.py
+++ calcolo_pressioni.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import matplotlib.pyplot as plt
-
 # Input dei dati della stratigrafia con i titoli degli strati
 stratigraphy = [
     {"top_level": 35, "bottom_level": 28, "unit_weight": 18, "k": 0.5, "title": "Remblais"},
